e0094ee2a3910aa2f19e7efe005b194cf7b1a3c7
Atsushi0915 <w_atsushi_0915@outlook.jp>
2022-08-15 07:03:52 +0900

    TaskCardTitleコンポーネント作成

--- src/components/task/TaskCardTitle.jsx
+++ src/components/task/TaskCardTitle.jsx
@@ -1,6 +1,43 @@
+import { useState } from "react"
+
 export const TaskCardTitle = () => {
+  const [ isClick, setIsClick ] = useState(false);
+  const [ inputCardTitle, setInputCArdTitle ] = useState("Todady")
+
+  const handleClick = () => {
+    setIsClick(true);
+  };
+
+  const handleChange = (e) => {
+    setInputCArdTitle(e.target.value)
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setIsClick(false)
+  };
+
+  const handleBlur = () =>{
+    setIsClick(false)
+  }
+
   return (
-    <>
-    </>
+    <div onClick ={handleClick} className="taskCardTitleInputArea" >
+      {isClick ? (
+        <form onSubmit={handleSubmit}>
+          <input 
+            className="taskCardTitleInput"
+            autoFocus
+            type="text" 
+            onChange={handleChange} 
+            onBlur={handleBlur} 
+            value={inputCardTitle}
+            maxLength={10}
+          />
+        </form> 
+      ) : ( 
+        <h3>{inputCardTitle}</h3> 
+      )}
+    </div>
   )
 }
